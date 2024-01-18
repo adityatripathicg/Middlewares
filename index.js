dfc1daa74949329268bceaa10eed1aa94846c229
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-01-19 00:59:44 +0530

    43 handling errors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res, next)=>{
-    console.log("Hi, I'm middleware");
+app.use("/random",(req,res, next)=>{
+    console.log("Hi, I'm only for random middleware");
     next();
 });
 //logger 
@@ -11,9 +11,27 @@ app.use((req,res, next)=>{
     console.log(req.method, req.path,req.responseTime, req.hostname);
     return next(); // next() and return next() both work same but return next() does not allow any code to run after return.
 });
+const checkToken = (req,res,next)=>{
+    let {token} = req.query;
+    if(token === "giveaccess"){
+        next();
+    }
+    throw new Error("Access Denied");
+};
+app.get("/api",checkToken, (req,res)=>{
+    res.send("Data");
+});
 
 app.get("/",(req,res)=>{
     res.send("Hello, I'm Root");
+});
+
+app.get("/wrong",(req,res)=>{
+    abcd = abcd;
+});
+//404
+app.get((req,res)=>{
+    res.send("Page Not Found");
 });
 
 app.listen(8080,()=>{
