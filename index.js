a64f97f94742eb50bcfb8f90574d65e31a398265
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-01-19 00:40:52 +0530

    logger  creating utility Middleware

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res)=>{
+app.use((req,res, next)=>{
     console.log("Hi, I'm middleware");
-    res.send("Middleware Finished!");
+    next();
+});
+//logger 
+app.use((req,res, next)=>{
+    req.responseTime = new Date(Date.now()).toString();
+    console.log(req.method, req.path,req.responseTime, req.hostname);
+    return next(); // next() and return next() both work same but return next() does not allow any code to run after return.
 });
 
 app.get("/",(req,res)=>{
